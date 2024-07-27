d2212f957f1e4486dab637d721179988037e9ba6
Michael Witbrock <witbrock@gmail.com>
2024-07-27 18:13:49 +1200

    Fix issue with test_get_client_1 test that was breaking wiht llama3.1

--- tests/test_vonlib/llmconnect_test.py
+++ tests/test_vonlib/llmconnect_test.py
@@ -1,6 +1,6 @@
 import unittest
 #from unittest.mock import patch
-from vonlib.llmconnect import ask_llm,get_client
+from vonlib.llmconnect import ask_llm,get_client, model_info
 
 class TestGpt4Connect(unittest.TestCase):
 
@@ -35,9 +35,12 @@ class TestGpt4Connect(unittest.TestCase):
         print(f"Test 2 Response: {response}")
         self.assertIn(expected_response,response )
 
+    # @TODO this test is probably due for retirement
+    # In the meantime, I've removed
+    # the explicit model specifier and let the get_client() function handle it via model_info()
     def test_get_client_1(self):#, mock_openai):
         response = get_client().chat.completions.create(
-           model="llama3",
+            model=model_info(),
             messages=[ # derived from ollama documentation 
                 {"role": "system", "content": "You are a question answering assistant."},
                 {"role": "user", "content": "Who won the US baseball world series in 2020?"},
