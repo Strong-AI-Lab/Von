6ac90030d3992088210458b4667ecb105c54db96
suyuchen <ysu132@aucklanduni.ac.nz>
2024-09-26 22:00:25 +1200

    Yuchen-9/26 Add the LLMs summary function and the classification

---
+++ src/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='vonlib',
+    packages=find_packages(include=['vonlib']),
+    version='0.1.0',
+    description='Python Package of tell_von',
+    author='YUchen SU',
+)
